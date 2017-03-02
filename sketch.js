50c02cac305558e64f3dc66779983d30b490fc10
Kimberlin Lazo Gonzales <klazogonzales@wlapcs.org>
2017-03-02 01:38:06 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function setup(){
+   createCanvas(600,400); 
+   background(100,100,100);
+}
+
+function draw(){
+    line(35,80,400,200);
+    line(25,60,100,300);
+    stroke(255,255,255);
+    fill(10,9,0);
+    line(55,70,200,150);
+    
+    stroke(255,255,255);
+    fill(255,0,0);
+    rect(290,100,250,250);
+    line(30,90,500,500);
+    
+    stroke(0,0,0);
+    strokeWeight(4);
+    fill(0,0,255);
+    ellipse(50,250,80,80);
+    ellipse(80,250,80,80);
+    ellipse(98,200,80,80);
+    
+    rect(250,100,200,250);
+    rect(200,200,200,200);
+}
+
+
